aded9f120da71af4f6bcfa81d94753a39d6348cf
Dmitriy.Struchkov <Dmitriy.Struchkov@portaone.com>
2015-07-25 03:59:27 +0300

    Imitate slow connection

--- wsgi/static/js/app/jj.js
+++ wsgi/static/js/app/jj.js
@@ -1,4 +1,11 @@
-angular.module('jjApp', [])
-    .controller('MainController', function() {
+  var app = angular.module("jjApp", []);
+  app.controller("mainController", ["$scope",
+      function($scope) {      }
+  ]);
 
-    });
+  var body = document.getElementsByTagName('body')[0];
+
+  setTimeout(function() {
+      body.setAttribute('ng-app', 'jjApp');
+      angular.bootstrap(body, ['ng', 'jjApp']);
+  }, 2000);
