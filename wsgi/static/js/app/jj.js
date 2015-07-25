8df350a8c5dc42f56c0344880a1c8d81d0ab6ca0
Dmitriy.Struchkov <Dmitriy.Struchkov@portaone.com>
2015-07-26 02:14:21 +0300

    Add the CSRF Token

--- wsgi/static/js/app/jj.js
+++ wsgi/static/js/app/jj.js
@@ -1,13 +1,62 @@
-  var app = angular.module("jjApp", []);
-  app.controller("MainController", ["$scope",
-      function($scope) {      }
-  ]);
-  app.controller("HeaderController", ["$scope",
-      function($scope) {      }
-  ]);
-  var body = document.getElementsByTagName('body')[0];
+JjApp.config(['$httpProvider',
+    function($httpProvider) {
+        $httpProvider.defaults.headers.common['X-CSRFToken'] = JjApp.csrf_token ;
+        $httpProvider.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=utf-8';
+        $httpProvider.defaults.transformRequest = [
 
-  setTimeout(function() {
-      body.setAttribute('ng-app', 'jjApp');
-      angular.bootstrap(body, ['ng', 'jjApp']);
-  }, 2000);
+            function(data) {
+                /**
+                 * The workhorse; converts an object to x-www-form-urlencoded serialization.
+                 * @param {Object} obj
+                 * @return {String}
+                 */
+                var param = function(obj) {
+                    var query = '';
+                    var name, value, fullSubName, subName, subValue, innerObj, i;
+
+                    for (name in obj) {
+                        value = obj[name];
+
+                        if (value instanceof Array) {
+                            for (i = 0; i < value.length; ++i) {
+                                subValue = value[i];
+                                fullSubName = name + '[' + i + ']';
+                                innerObj = {};
+                                innerObj[fullSubName] = subValue;
+                                query += param(innerObj) + '&';
+                            }
+                        } else if (value instanceof Object) {
+                            for (subName in value) {
+                                subValue = value[subName];
+                                fullSubName = name + '[' + subName + ']';
+                                innerObj = {};
+                                innerObj[fullSubName] = subValue;
+                                query += param(innerObj) + '&';
+                            }
+                        } else if (value !== undefined && value !== null) {
+                            query += encodeURIComponent(name) + '=' + encodeURIComponent(value) + '&';
+                        }
+                    }
+
+                    return query.length ? query.substr(0, query.length - 1) : query;
+                };
+
+                return angular.isObject(data) && String(data) !== '[object File]' ? param(data) : data;
+            }
+        ];
+    }
+]);
+
+JjApp.controller("MainController", ["$scope",
+    function($scope) {}
+]);
+JjApp.controller("HeaderController", ["$scope",
+    function($scope) {}
+]);
+
+var body = document.getElementsByTagName('body')[0];
+
+setTimeout(function() {
+    body.setAttribute('ng-app', 'jjApp');
+    angular.bootstrap(body, ['ng', 'jjApp']);
+}, 2000);
