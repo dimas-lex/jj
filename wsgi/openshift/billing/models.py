93a8060a893ce11b455f6525058451d949b5cfce
Dmitriy.Struchkov <Dmitriy.Struchkov@portaone.com>
2015-07-26 23:11:29 +0300

    Add nullable to model's fields

--- wsgi/openshift/billing/models.py
+++ wsgi/openshift/billing/models.py
@@ -3,19 +3,19 @@ from django.db import models
 from datetime import date
 
 class Address(models.Model):
-    address1 = models.CharField(max_length=50)
-    city = models.CharField(max_length=50)
-    state = models.CharField(max_length=50)
-    zipcode = models.CharField(max_length=10)
+    address1 = models.CharField(max_length=50,  null=True)
+    city = models.CharField(max_length=50,  null=True)
+    state = models.CharField(max_length=50,  null=True)
+    zipcode = models.CharField(max_length=10,  null=True)
 
 class Customer(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     email = models.CharField(max_length=50)
-    is_active = models.BooleanField(default=True)
+    is_active = models.BooleanField(default=True,  null=True)
     balance = models.DecimalField (max_digits=10, decimal_places=5, default=0)
-    home_address = models.ForeignKey(Address)
-    mailing_address = models.ForeignKey(Address)
+    home_address = models.ForeignKey(Address,  null=True)
+    mailing_address = models.ForeignKey(Address,  null=True)
 
 class Invoice(models.Model):
     customer = models.ForeignKey(Customer)
