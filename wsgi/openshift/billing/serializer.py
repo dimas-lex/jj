6f6bed044ad7076c2c6a984f16e55402bea0b131
Dmitriy.Struchkov <Dmitriy.Struchkov@portaone.com>
2015-07-27 00:12:25 +0300

    Change serializer

--- wsgi/openshift/billing/serializer.py
+++ wsgi/openshift/billing/serializer.py
@@ -1,26 +1,26 @@
-from billing.models import *
-from rest_framework import serializers
-import json
+# from billing.models import *
+# from rest_framework import serializers
+# import json
 
 
-class AddressSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Address
-        fields = ('id', 'address1','city', 'state', 'zipcode')
+# class AddressSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Address
+#         fields = ('id', 'address1','city', 'state', 'zipcode')
 
 
-class CustomerSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Customer
-        fields = ('id', 'first_name','last_name', 'email', 'is_active', 'balance',
-            'home_address', 'mailing_address')
+# class CustomerSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Customer
+#         fields = ('id', 'first_name','last_name', 'email', 'is_active', 'balance',
+#             'home_address', 'mailing_address')
 
-class InvoiceSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Invoice
-        fields = ('id', 'customer','amount')
+# class InvoiceSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Invoice
+#         fields = ('id', 'customer','amount')
 
-class PaymentSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Invoice
-        fields = ('id', 'customer','amount')
+# class PaymentSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Invoice
+#         fields = ('id', 'customer','amount')
--- wsgi/static/js/app/jj.js
+++ wsgi/static/js/app/jj.js
@@ -4,6 +4,7 @@ JjApp.config(['$httpProvider',
     function($httpProvider) {
         $httpProvider.defaults.headers.common['X-CSRFToken'] = JjApp.csrf_token;
         $httpProvider.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=utf-8';
+        $httpProvider.defaults.headers.common["X-Requested-With"] = 'XMLHttpRequest';
         $httpProvider.defaults.transformRequest = [
 
             function(data) {
