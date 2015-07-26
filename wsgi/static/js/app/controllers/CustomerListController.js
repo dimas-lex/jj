7e74ac14d8f85b50bf57fef6dd688208e8aae348
Dmitriy.Struchkov <Dmitriy.Struchkov@portaone.com>
2015-07-27 01:13:34 +0300

    Alerts

--- wsgi/static/js/app/controllers/CustomerListController.js
+++ wsgi/static/js/app/controllers/CustomerListController.js
@@ -1,4 +1,4 @@
-'use strict';
+angular.module('ng').filter('tel', function() {});
 JjApp.controller('CustomerListController', ['$scope', "dataExchangeService", '$http',
     function($scope) {
         $scope.customers = [{
@@ -21,41 +21,15 @@ JjApp.controller('CustomerListController', ['$scope', "dataExchangeService", '$h
             balance: 10
         }];
 
-        $scope.loadData = function() {
-        };
+        $scope.loadData = function() {};
         $scope.$on('reloadCustomers', function() {
             $scope.loadData();
         });
         // $scope.loadData();
     }
 ])
-.directive('customerList',["Constants", function() {
-    return {
-        templateUrl: '/static/js/app/templates/forms/customer_list.html'
-        // template: '<div class="panel-body">' +
-        //     '    <h2>Customer Info</h2>' +
-        //     '    <table class="table table-striped table-bordered table-hover" >' +
-        //     '        <thead>' +
-        //     '          <tr>' +
-        //     '              <th width="20%">First Name</th>' +
-        //     '              <th width="20%">Last Name</th>' +
-        //     '              <th width="30%">Email</th>' +
-        //     '              <th >Balance</th>' +
-        //     '              <th >Active</th>' +
-        //     '          </tr>' +
-        //     '        </thead>' +
-        //     '        <tbody>' +
-        //     '            <tr ng-repeat="customer in customers | orderBy:\'-firts_name\'">' +
-        //     '                <td data-name="first_name">{$customer.first_name$}</td>' +
-        //     '                <td data-name="last_name">{$customer.last_name$}</td>' +
-        //     '                <td data-name="email">{$customer.email$}</td>' +
-        //     '                <td data-name="balance">{$customer.balance$}</td>' +
-        //     '                <td data-name="is_active">' +
-        //     '                   <img ng-src="{$ customer.is_active && \'/home/dimas/code/jj/wsgi/static/resources/images/yes_tick.png\'  || \'/home/dimas/code/jj/wsgi/static/resources/images/round_remove.png\'$}"/> </td>' +
-        //     '            </tr>' +
-        //     '        </tbody>' +
-        //     '    </table>' +
-        //     '</div>',
-
-    };
-}]);
+    .directive('customerList', function() {
+        return {
+            templateUrl: '/static/js/app/templates/forms/customer_list.html'
+        };
+    });
