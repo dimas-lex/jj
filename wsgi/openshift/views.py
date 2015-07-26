8728a57cc3ed59620f3c1ca71dce475e561b1722
Dmitriy.Struchkov <Dmitriy.Struchkov@portaone.com>
2015-07-27 00:16:01 +0300

    Change serializer

--- wsgi/openshift/views.py
+++ wsgi/openshift/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render_to_response
 from django.http import HttpResponse, Http404
 from django.template.context import RequestContext
+from django.core import serializers
 
 from billing.models import *
-from serializers import *
 
 def index(request):
      return render_to_response('home/index.html')
@@ -13,3 +13,24 @@ def get_customers(self, request):
     customers = Customer.objects.all()
     data = serializers.serialize('json', customers)
     return HttpResponse(data, mimetype='application/json')
+
+def post(self, request):
+
+    if 'id' in request.POST:
+#         # update
+#         customer = self.get_object(request.POST.get('id'))
+#         if customer:
+#             serializer = CustomerSerializer(customer, data=request.DATA)
+#             if serializer.is_valid():
+#                 serializer.save()
+#                 return Response(serializer.data , status=status.HTTP_201_CREATED)
+
+    else:
+#         # add new Customer
+        serializer = serializers.deserialize(data=request.DATA)
+#         if serializer.is_valid():
+#             serializer.save()
+            return Response(serializer.data)
+
+#     # fail
+    # return Response({'success': False}, status=status.HTTP_400_BAD_REQUEST)
