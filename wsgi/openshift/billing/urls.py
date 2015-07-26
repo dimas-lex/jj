4be0f4896ce8806e828241954b2f6366dd60f9d7
Dmitriy.Struchkov <Dmitriy.Struchkov@portaone.com>
2015-07-26 23:44:30 +0300

    Add Serializers, Change urls.py

--- wsgi/openshift/billing/urls.py
+++ wsgi/openshift/billing/urls.py
@@ -1,5 +1,9 @@
 from django.conf.urls.defaults import patterns, include, url
+from django.conf.urls import patterns, include, url
+from django.conf import settings
 
+from views import CustomerListView
 
-urlpatterns = patterns('',
+urlpatterns = format_suffix_patterns(
+    ( url(r'^rest/customer/$',  CustomerListView.as_view()), )
 )
--- wsgi/openshift/urls.py
+++ wsgi/openshift/urls.py
@@ -1,12 +1,12 @@
 from django.conf.urls import patterns, include, url
-from django.contrib import admin
+# from django.contrib import admin
 from django.conf import settings
 
 from billing.views import CustomerListView
 from django.views.generic import TemplateView
 from rest_framework.urlpatterns import format_suffix_patterns
 
-admin.autodiscover()
+# admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^$', 'views.index', name='index'),
