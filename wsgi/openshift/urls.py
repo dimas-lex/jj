f866169dc6fcfafb54567b6c5d92a5f012997e55
Dmitriy.Struchkov <Dmitriy.Struchkov@portaone.com>
2015-07-27 02:04:36 +0300

    Add alert box

--- wsgi/openshift/urls.py
+++ wsgi/openshift/urls.py
@@ -4,7 +4,7 @@ from django.views.generic import TemplateView
 # from django.contrib import admin
 # from rest_framework.urlpatterns import format_suffix_patterns
 # admin.autodiscover()
-from views import get_customers
+
 
 urlpatterns = patterns('',
     # url(r'^rest/customer/$', get_customers),
