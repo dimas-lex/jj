e140af1c382a73a4b19eff8e5c1cd83891a12a03
Dmitriy.Struchkov <Dmitriy.Struchkov@portaone.com>
2015-07-26 22:44:46 +0300

    Constants

---
+++ wsgi/static/js/app/Constants.js
@@ -0,0 +1,9 @@
+'use strict';
+JjApp.constant("Constants", {
+    img: {
+        YES: '/home/dimas/code/jj/wsgi/static/resources/images/yes_tick.png',
+        NO: '/home/dimas/code/jj/wsgi/static/resources/images/round_remove.png',
+        round_remove: '/home/dimas/code/jj/wsgi/static/resources/images/round_remove.png'
+    }
+
+});
