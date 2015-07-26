cee79806a03b245f093b410c561329a7e8e59e82
Dmitriy.Struchkov <Dmitriy.Struchkov@portaone.com>
2015-07-26 23:07:41 +0300

    Add Address model

--- wsgi/openshift/billing/models.py
+++ wsgi/openshift/billing/models.py
@@ -2,7 +2,11 @@
 from django.db import models
 from datetime import date
 
-
+class Address(models.Model):
+    address1 = models.CharField(max_length=50)
+    city = models.CharField(max_length=50)
+    state = models.CharField(max_length=50)
+    zipcode = models.CharField(max_length=10)
 
 class Customer(models.Model):
     first_name = models.CharField(max_length=50)
@@ -10,7 +14,8 @@ class Customer(models.Model):
     email = models.CharField(max_length=50)
     is_active = models.BooleanField(default=True)
     balance = models.DecimalField (max_digits=10, decimal_places=5, default=0)
-
+    home_address = models.ForeignKey(Address)
+    mailing_address = models.ForeignKey(Address)
 
 class Invoice(models.Model):
     customer = models.ForeignKey(Customer)
