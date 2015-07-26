99408b108e6e3d023d51a1169d980a0297d3899a
Dmitriy.Struchkov <Dmitriy.Struchkov@portaone.com>
2015-07-26 23:38:54 +0300

    Add Address model

---
+++ wsgi/openshift/billing/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render_to_response
+from django.http import HttpResponse, Http404
+from django.template.context import RequestContext
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import viewsets, status, generics, permissions
+
+from billing.models import *
+from billing.serializers import *
+
+
+class CustomerListView(APIView):
+    """
+        View class for managing and viewing Account
+    """
+
+    def get(self, request):
+        customers = Customer.objects.all()
+        serializer = CustomerSerializer(customers, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+
+        if 'id' in request.POST:
+            # update
+            customer = self.get_object(request.POST.get('id'))
+            if customer:
+                serializer = CustomerSerializer(customer, data=request.DATA)
+                if serializer.is_valid():
+                    serializer.save()
+                    return Response(serializer.data , status=status.HTTP_201_CREATED)
+
+        else:
+            # add new Customer
+            serializer = CustomerSerializer(data=request.DATA)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data)
+
+        # fail
+        return Response({'success': False}, status=status.HTTP_400_BAD_REQUEST)
--- wsgi/openshift/urls.py
+++ wsgi/openshift/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.conf import settings
 
-from billing.views import *
+from billing.views import CustomerListView
 from django.views.generic import TemplateView
 from rest_framework.urlpatterns import format_suffix_patterns
 
